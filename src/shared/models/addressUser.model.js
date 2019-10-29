9092b9d5fcfaba8a81cf47882575d5c0aa9595c6
HiTa-life <hinda.talaron@gmail.com>
2019-10-29 16:58:05 +0100

    class creation

---
+++ src/shared/models/addressUser.model.js
@@ -0,0 +1,10 @@
+export class AddressUser {
+    constructor (  address, 
+        city, 
+        zipCode)
+        {
+            this.address = address;
+            this.city = city;
+            this.zipCode = zipCode
+        };
+};
