e7982868aa6e50e2bedb354fba0ad60e0e4ceda2
HiTa-life <hinda.talaron@gmail.com>
2019-10-30 09:29:30 +0100

    name of file is changed

--- src/shared/models/user.model.js
+++ src/shared/models/user.model.js
@@ -1,5 +1,5 @@
 import { MailUser } from "./mailUser.model";
-import { NameUser } from "./nameUser.models";
+import { NameUser } from "./nameUser.model";
 import { AddressUser } from "./addressUser.model";
 import { PhoneUser } from "./phoneUser.model";
 import { Password } from "./passwordUser.model";
