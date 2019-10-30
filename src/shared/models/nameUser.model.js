4b9e0048ae3c7584dba4127b38356ba5f2d3b3f6
HiTa-life <hinda.talaron@gmail.com>
2019-10-30 09:29:05 +0100

    name of file is changed

---
+++ src/shared/models/nameUser.model.js
@@ -0,0 +1,12 @@
+export class NameUser {
+    constructor( surName, 
+        firstName, 
+        lastName, 
+        ) 
+        {
+        this.surName = surName; 
+        this.firstName = firstName;
+        this.lastName = lastName;
+       
+        };
+};
