b1b021f6989ef57381c19c5d14a07703b45312dd
HiTa-life <hinda.talaron@gmail.com>
2019-11-04 11:45:08 +0100

    update code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import 'bootstrap';
-import {LoginComponent} from "./app/login/login-component/login.component";
+//import {LoginComponent} from "./app/login/login-component/login.component";
+import { RegisterComponent } from './app/register/register-component';
 
-const login = new LoginComponent();
-login.display();
+
+// const login = new LoginComponent();
+// login.display();
+
+const register = new RegisterComponent();
+register.display();
