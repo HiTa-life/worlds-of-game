e8c1fb6e2587e24d99afe212208669280be2e782
HiTa-life <hinda.talaron@gmail.com>
2019-11-05 10:18:41 +0100

    attributes modified

--- src/shared/models/user/user.model.js
+++ src/shared/models/user/user.model.js
@@ -1,18 +1,18 @@
-import { MailUser } from "./mail-user.model";
-import { NameUser } from "./name-user.model";
-import { AddressUser } from "./address-user.model";
-import { PhoneUser } from "./phone-user.model";
-import { Password } from "./password-user.model";
 
 
 export class User {
 
-    constructor() {
-        // this.nameUser = new NameUser();
-        // this.mailUser = new MailUser();
-        // this.addressUser = new AddressUser();
-        // this.phoneUser = new PhoneUser();
-        // this.password = new Password();
+    constructor(email,password,surname,firstName,lastName,phone,adress,city,zip) {
+        this.email = email;
+        this.password = password; 
+        this.surname = surname;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone; 
+        this.adress = adress;
+        this.city = city;
+        this.zip = zip;
+      
 
     }  
 
