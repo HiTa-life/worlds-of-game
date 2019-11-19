358f29e3f81f4215d49b994e979699519ee3d770
HiTa-life <56936279+HiTa-life@users.noreply.github.com>
2019-11-19 21:28:41 +0100

    Update register-button.component.js

--- src/app/login/register-button/register-button.component.js
+++ src/app/login/register-button/register-button.component.js
@@ -3,18 +3,21 @@ import { Component } from "../../../shared/components/component";
 export class RegisterButtonComponent extends Component {
   constructor(textButtonRegister) {
     super();
-    this.textButtonRegister = textButtonRegister;
+    this.textButtonRegister = null;
     this.buttonRegister;
    
   }
 
 
-  display(div1) {
-    const buttonRegisterLink = super.createAppendElement(div1, "a");
+  display(parent) {
+    this.element = super.createAppendElement("app-button-register")
+    this.buttonRegisterLink = super.createAppendElement(this.element, "a");
     this.buttonRegister = super.createAppendElement(buttonRegisterLink, "button");
     super.createAppendTextNode(this.buttonRegister, this.textButtonRegister);
     super.setAttribute(this.buttonRegister, { class: "buttonRegister", type: "submit" });
     super.setAttribute(buttonRegisterLink, { href:"" });
+    document.body.appendChild(this.element);
   }
-
-}
+   hide() {
+        document.body.removeChild(this.element);
+    }
