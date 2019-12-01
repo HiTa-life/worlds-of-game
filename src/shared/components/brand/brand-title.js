995a8b413b165611ffc260df1f5db04eadb7e205
HiTa-life <56936279+HiTa-life@users.noreply.github.com>
2019-12-01 11:33:58 +0100

    Update brand-title.js

--- src/shared/components/brand/brand-title.js
+++ src/shared/components/brand/brand-title.js
@@ -16,18 +16,14 @@ export class BrandTitle extends Component {
     this.element = document.createElement("wog-brand-title")
     this.title = super.createAppendElement(this.element, "h1"); 
     this.textTitle = super.createAppendTextNode(this.title, this.textTitle);    
-    this.title.display(this.element); 
     this.img = super.createAppendElement(this.element, "img");
     super.setAttribute(this.img, { class: "imgLogo", src: "assets/images/logo-color.png", alt: "logo Worlds of  game", width : "100px" });
-    this.img.display(this.element);
-    document.body.appendChild(this.element);
+    div1.appendChild(this.element);
 
   }
 
   hide(){
-    this.title.hide();
-    this.textTitle.hide();
-    this.img.hide();
+  
     this.element.parentNode.removeChild(this.element);
   }
 
